ed5a424fc3e0e0c5de74ead6b145696e3865dcfc
Valeriy Novytskyy <valeriy.novytskyy@outlook.com>
2024-04-30 19:34:29 -0700

    Minor edits for readability

--- simulate.m
+++ simulate.m
@@ -1,3 +1,9 @@
+global A;
+global B;
+global C;
+global D;
+global K;
+
 % A weights (3x3 matrix)
 A = [ ...
   0.9988,     0.05193, -0.02261;
@@ -30,32 +36,42 @@ K = [ ...
 ];
 
 % Initial state (3x1 vector)
-x0 = [ ...
+initialState = [ ...
   -0.0458;
   0.0099;
   -0.0139;
 ];
 
+% Disturbance
+disturbance = 0;
+
 % Read input
 csv = readmatrix('https://raw.githubusercontent.com/01binary/systemid/main/input.csv');
 time = csv(:,1);
-measurement = csv(:,2);
-input = csv(:,3);
+measurements = csv(:,2);
+inputs = csv(:,3);
+outputs = zeros(length(inputs), 1);
 
 % Simulate
-x = x0;
-output = zeros(length(input), 1);
+systemState = initialState;
 
-for i = 1:length(input)
-  u = input(i);
-  [y, x] = systemModel(A, B, C, D, K, x, u, 0);
-  output(i) = y;
+for i = 1:length(inputs)
+  input = inputs(i);
+  [prediction, systemState] = systemModel(systemState, input, disturbance);
+  outputs(i) = prediction;
 end
 
 % Plot
-plot(time, measurement, time, output);
+plot(time, measurements, time, outputs);
 
-function [y, x] = systemModel(A, B, C, D, K, x, u, e)
+% Linear discrete system model
+function [y, x] = systemModel(x, u, e)
+  global A;
+  global B;
+  global C;
+  global D;
+  global K;
+
   % y = Cx + Du + e
   y = ...
     C * x + ...  % Add contribution of state
